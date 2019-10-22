6091786fa668f384b910f38179bd01fab2b149d0
Courtney Jackson <52590162+CoderZim@users.noreply.github.com>
2019-10-21 23:32:33 -0400

    working todo functionality

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -6,6 +6,7 @@ import PartyContext from "./contexts/partyContext";
 import ShoppingContext from "./contexts/shoppingContext";
 
 import RegisterUser from "./components/register";
+import TodoList from "./components/toDoList";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Router>
             {/* <Route exact path="/login" component={login} /> */}
             <Route exact path="/register" component={RegisterUser} />
+            <Route path="/todo" component = {TodoList}/>
           </Router>
         </div>
       </PartyContext.Provider>
