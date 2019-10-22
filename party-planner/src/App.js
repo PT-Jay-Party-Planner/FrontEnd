e7e737c752b6c559f62fe3abc2a0aae022be8e57
RyanSWest <west56309@gmail.com>
2019-10-21 21:44:43 -0500

    shopping-list

--- party-planner/src/App.js
+++ party-planner/src/App.js
@@ -1,19 +1,63 @@
-import React from "react";
+import React ,{useState} from "react";
 import "./App.css";
 
-import PartyContext from "./contexts/partyContext";
-import ShoppingContext from "./contexts/shoppingContext";
-
+import {PartyContext} from "./contexts/partyContext";
+import {ShoppingContext} from "./contexts/shoppingContext";
+import data from '../src/data'
+import AddShoppingList from "./components/addShoppingList";
+import AddParty from  './components/addParty';
+import Party from './components/partyForm';
+import ShoppingForm from './components/shoppingForm';
+import {BrowserRouter as Router, Route} from "react-router-dom";
 function App() {
+ 
+
+   
+  const initialList =[]
+
+  const shoppingList = [{ item:'BackFatt', price:6900}]
+  const [items] = useState(shoppingList)
+   
+
+   const [list, setList] =useState([]);
+   console.log ("ITEMS FROM APP", items)
+   const [party, setParty] = useState([]);
+
+   const addItem = item =>{
+     setList([...items, item])
+
+      // const [partyEdit, setPartyEdit] = useState([]);
+   }
+
   return (
-    <ShoppingContext.Provider>
-      <PartyContext.Provider>
-        <div className="App">
-          <header className="App-header"></header>
-        </div>
-      </PartyContext.Provider>
-    </ShoppingContext.Provider>
-  );
+  
+<div> 
+
+<PartyContext.Provider value ={{party,setParty}}>
+ 
+ 
+ <Route path ="/" component = {AddParty}/>
+  
+       
+</PartyContext.Provider>
+
+
+<ShoppingContext.Provider value ={{items}}>
+
+  <Route path ="/shopping" component ={AddShoppingList}/>
+</ShoppingContext.Provider>
+
+ 
+</div>
+ 
+
+ 
+
+
+   
+
+      
+   );
 }
 
 export default App;
--- party-planner/src/index.js
+++ party-planner/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.less";
+import'./design/index.less';
 import App from "./App";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+ <Router>
+
+<App/>
+ </Router>,
+   document.getElementById("root"));
